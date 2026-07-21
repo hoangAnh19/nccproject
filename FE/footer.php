8963431d72900014e7e6b0f8272bafb9e5f5c5fe
anhth <thabka99@gmail.com>
2026-07-21 17:19:44 +0700

    clear-project

--- FE/footer.php
+++
@@ -1,6 +0,0 @@
-<footer></footer>
-<script type='text/javascript' src='assets/lib/js/jquery.min.js'></script>
-<script type="text/javascript" src="assets/js/private.js?v=<?php echo time() ?>"></script>
-</body>
-
-</html>
